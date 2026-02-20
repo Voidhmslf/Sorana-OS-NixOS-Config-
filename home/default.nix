bc0fd4044939f4d9cd5e6e798f7b1541e3c5f491
void <tamarazaliev@gmail.com>
2026-02-20 16:17:42 +0000

    feat: Add zsh, fzf, and plugins

--- home/default.nix
+++ home/default.nix
@@ -1,14 +1,46 @@
-{ config,pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
   home.username = "void";
   home.homeDirectory = "/home/void";
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
-  xdg.configFile."hypr/hyprland.conf".source = ./desktop/hyprland/hyprland.conf;
-  
+
+  # Импортируем остальные части конфигурации
   imports = [
     ./programs/default.nix
     ./terminal/kitty.nix
   ];
+
+  # --- Новая конфигурация для Zsh и интерактивных утилит ---
+
+  # Устанавливаем Zsh как оболочку по умолчанию
+  users.defaultUserShell = pkgs.zsh;
+
+  # Включаем fzf (fuzzy finder) и его интеграцию с zsh
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  # Настраиваем Zsh
+  programs.zsh = {
+    enable = true;
+    # Включаем Oh My Zsh для плагинов и тем
+    oh-my-zsh = {
+      enable = true;
+      # Устанавливаем тему (robbyrussell - стандартная и чистая, хорошо для начала)
+      theme = "robbyrussell";
+      # Добавляем плагины для улучшения опыта
+      plugins = [
+        "git"                     # Полезные сокращения для Git
+        "fzf"                     # Интеграция с fzf (Ctrl+R для истории)
+        "zsh-autosuggestions"     # Предложения команд на лету
+        "zsh-syntax-highlighting" # Подсветка синтаксиса вводимых команд
+      ];
+    };
+  };
+
+  # Копируем конфиг hyprland
+  xdg.configFile."hypr/hyprland.conf".source = ./desktop/hyprland/hyprland.conf;
 }
